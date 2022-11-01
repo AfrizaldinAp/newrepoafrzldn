461e77a20bf7afaa3dbf08f4d539fc5cc0447c46
caca aldi <caca.aldi10@gmail.com>
2022-11-01 13:04:12 +0700

    menambahkan navbar login

--- resources/views/parent.blade.php
+++ resources/views/parent.blade.php
@@ -52,6 +52,9 @@
             <li class="nav-item">
               <a class="nav-link active" href="/contact">Contact</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/login">Login</a>
+            </li>
           </ul>
         </div>
       </div>
